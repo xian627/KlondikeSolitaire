6bee48928018dd370d1a7f115a9e94baa8395283
Xuan <xian627@yahoo.com>
2018-09-14 09:16:50 -0700

    Initial Commit

---
+++ Assets/Klondike Solitaire/Scripts/Utilities/CanvasScaleFactorConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScaleFactorConverter  {
+
+    private static Canvas gameCanvas;
+
+	public static float MultiplyByScaleFactor(float value)
+    {
+        if(CheckIfCanvasIsEmpty())
+        {
+            Debug.LogError("Empty canvas!");
+            return 0;
+        }
+        return value * gameCanvas.scaleFactor;
+    }
+
+
+    public static float DivideByScaleFactor(float value)
+    {
+        if (CheckIfCanvasIsEmpty())
+        {
+            Debug.LogError("Empty canvas!");
+            return 0;
+        }
+        return value / gameCanvas.scaleFactor;
+    }
+
+    private static bool CheckIfCanvasIsEmpty()
+    {
+        if (gameCanvas == null)
+        {
+            GameObject canvasGO = GameObject.FindGameObjectWithTag("GameCanvas");
+            if (canvasGO != null)
+            {
+                gameCanvas = canvasGO.GetComponent<Canvas>();
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
